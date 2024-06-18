95e24f11c5ff8caba7b139f6177e2d054fdac048
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-06-18 10:37:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-git clone https://github.com//wordpress.git
+git clone https://github.com/kaasu-pavani/wordpress.git
 cd wordpress/
 docker-compose up -d
